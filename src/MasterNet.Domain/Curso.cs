e9540266e58f52dd1c16ea07d852a183d80ed3db
Marco Cruz <marco24cruz08@gmail.com>
2024-08-14 22:52:12 -0600

    ✨ Add FechaPublicacion property to Curso class
    The code changes add a new property `FechaPublicacion` to the `Curso` class in the `MasterNet.Domain` project. This property represents the publication date of a course. This modification is necessary to track the publication date of each course in the application.

--- src/MasterNet.Domain/Curso.cs
+++ src/MasterNet.Domain/Curso.cs
@@ -4,6 +4,7 @@ public class Curso : BaseEntity
 {
   public string? Titulo { get; set; }
   public string? Descripcion { get; set; }
+  public DateTime FechaPublicacion { get; set; }
   public ICollection<Calificacion>? Calificaciones { get; set; }
   public ICollection<Precio>? Precios { get; set; }
   public ICollection<CursoPrecio>? CursoPrecios { get; set; }
--- src/MasterNet.Persistence/MasterNetDbContext.cs
+++ src/MasterNet.Persistence/MasterNetDbContext.cs
@@ -1,3 +1,4 @@
+using Bogus;
 using MasterNet.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -6,6 +7,11 @@ namespace MasterNet.Persistence;
 
 public class MasterNetDbContext : DbContext
 {
+  public DbSet<Curso> Cursos { get; set; }
+  public DbSet<Instructor> Instructores { get; set; }
+  public DbSet<Precio> Precios { get; set; }
+  public DbSet<Calificacion> Calificaciones { get; set; }
+
   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
     optionsBuilder.UseSqlite("Data Source=LocalDatabase.db")
@@ -83,5 +89,54 @@ public class MasterNetDbContext : DbContext
           .HasForeignKey(p => p.CursoId),
         j => { j.HasKey(t => new { t.CursoId, t.InstructorId }); } // Llave primaria compuesta
       );
+
+    modelBuilder.Entity<Curso>()
+      .HasData(DataMaster().Item1);
+
+    modelBuilder.Entity<Precio>()
+      .HasData(DataMaster().Item2);
+
+    modelBuilder.Entity<Instructor>()
+      .HasData(DataMaster().Item3);
+  }
+
+  public Tuple<Curso[], Precio[], Instructor[]> DataMaster()
+  {
+    var cursos = new List<Curso>();
+    var faker = new Faker();
+
+    for (var i = 1; i < 10; i++)
+    {
+      var cursoId = Guid.NewGuid();
+      cursos.Add(
+        new Curso
+        {
+          Id = cursoId,
+          Descripcion = faker.Commerce.ProductDescription(),
+          Titulo = faker.Commerce.ProductName(),
+          FechaPublicacion = DateTime.UtcNow,
+        }
+      );
+    }
+
+    var precioId = Guid.NewGuid();
+    var precio = new Precio
+    {
+      Id = precioId,
+      PrecioActual = 10.0m,
+      PrecioPromocion = 8.0m,
+      Nombre = "Precio regular",
+    };
+    var precios = new List<Precio> { precio };
+
+    var fakerInstructor = new Faker<Instructor>()
+      .RuleFor(t => t.Id, _ => Guid.NewGuid())
+      .RuleFor(t => t.Nombre, f => f.Name.FullName())
+      .RuleFor(t => t.Apeliidos, f => f.Name.LastName())
+      .RuleFor(t => t.Grado, f => f.Name.JobTitle());
+
+    var instructores = fakerInstructor.Generate(10);
+
+    return Tuple.Create(cursos.ToArray(), precios.ToArray(), instructores.ToArray());
   }
 }
