3980fe0223222d67ebcc61b0e4c598ea60eb6ef4
Marco Cruz <marco24cruz08@gmail.com>
2024-08-14 23:27:25 -0600

    ✨ Add MainEntry.cs file and implement course creation and retrieval
    The code changes add a new file `MainEntry.cs` to the `MasterNet.Persistence` project. This file contains code for creating a new course and retrieving all courses from the database. This functionality is necessary for managing courses in the application.

---
+++ src/MasterNet.Persistence/MainEntry.cs
@@ -0,0 +1,24 @@
+using MasterNet.Domain;
+using MasterNet.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello World!");
+
+using var context = new MasterNetDbContext();
+
+var newCurso = new Curso
+{
+  Id = Guid.NewGuid(),
+  Titulo = "Curso de .NET 9",
+  Descripcion = "Bases de programación en .NET 9",
+  FechaPublicacion = DateTime.Now
+};
+
+context.Add(newCurso);
+await context.SaveChangesAsync();
+
+var cursos = await context.Cursos.ToListAsync();
+foreach (var curso in cursos)
+{
+  Console.WriteLine($"{curso.Id} : {curso.Titulo}");
+}
