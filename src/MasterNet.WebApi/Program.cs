5be20eb81a0aa61708cac4ee47098dcafd55a652
Marco Cruz <marco24cruz08@gmail.com>
2024-08-15 17:18:32 -0600

    ✏️ Refactor DataSeed.cs to improve data seeding process
    The code changes in DataSeed.cs refactor the data seeding process to improve efficiency and readability. The changes include adding necessary using statements, migrating the database, creating admin and client users with roles, associating instructors with courses, assigning prices to courses, generating fake ratings for courses, and saving changes to the database. This refactor enhances the data seeding functionality in the application.

--- src/MasterNet.WebApi/Program.cs
+++ src/MasterNet.WebApi/Program.cs
@@ -1,6 +1,8 @@
 using MasterNet.Application;
 using MasterNet.Persistence;
+using MasterNet.Persistence.Models;
 using MasterNet.WebApi.Extensions;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,12 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddIdentityCore<AppUser>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.User.RequireUniqueEmail = true;
+}).AddRoles<IdentityRole>().AddEntityFrameworkStores<MasterNetDbContext>();
 
 var app = builder.Build();
 
